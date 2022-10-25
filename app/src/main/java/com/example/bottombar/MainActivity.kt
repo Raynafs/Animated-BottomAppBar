7de5ccfa5481f014a4d66b2a2e0b31f114782d7d
NOAH <noahmuhindi11@gmail.com>
2022-10-25 20:33:31 +0100

    Bottom app bar icon

--- app/src/main/java/com/example/bottombar/MainActivity.kt
+++ app/src/main/java/com/example/bottombar/MainActivity.kt
@@ -3,10 +3,15 @@ package com.example.bottombar
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import com.example.bottombar.ui.theme.BottomBarTheme
 
 class MainActivity : ComponentActivity() {
@@ -30,8 +35,29 @@ class MainActivity : ComponentActivity() {
 fun BottomBarr()
 {
     Scaffold(
+
         bottomBar = {
-            BottomAppBar{
+
+            BottomAppBar(
+                contentPadding = PaddingValues(start = 20.dp, end= 20.dp) ,
+                backgroundColor = Color.White,
+                elevation = 20.dp
+            )
+            {
+
+                Image(painterResource(id = R.drawable.smile),
+                    contentDescription = "smiling emoji" ,
+
+                    modifier= Modifier.size(24.dp),
+                colorFilter = ColorFilter.tint(Color.Black))
+
+                Spacer(modifier = Modifier.height(60.dp))
+
+                Image(painterResource(id = R.drawable.user),
+                    contentDescription = "user" ,
+
+                    modifier= Modifier.size(20.dp))
+
             }
         }
     )

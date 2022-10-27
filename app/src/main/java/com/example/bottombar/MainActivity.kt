6f59a5acc8a4b6cd78299868e625cc040b12df3b
NOAH <noahmuhindi11@gmail.com>
2022-10-27 19:36:14 +0100

    BottomNav and FloatingActionButton

--- app/src/main/java/com/example/bottombar/MainActivity.kt
+++ app/src/main/java/com/example/bottombar/MainActivity.kt
@@ -7,12 +7,16 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.example.bottombar.ui.theme.BottomBarTheme
+import kotlin.math.absoluteValue
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,7 +28,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                  BottomBarr()
+                  BottomBarScaffold()
                 }
             }
         }
@@ -32,37 +36,46 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun BottomBarr()
+fun BottomBarScaffold()
 {
     Scaffold(
-
-        bottomBar = {
-
-            BottomAppBar(
-                contentPadding = PaddingValues(start = 20.dp, end= 20.dp) ,
-                backgroundColor = Color.White,
-                elevation = 20.dp
-            )
-            {
-
-                Image(painterResource(id = R.drawable.smile),
-                    contentDescription = "smiling emoji" ,
-
-                    modifier= Modifier.size(24.dp),
-                colorFilter = ColorFilter.tint(Color.Black))
-
-                Spacer(modifier = Modifier.height(60.dp))
-
-                Image(painterResource(id = R.drawable.user),
-                    contentDescription = "user" ,
-
-                    modifier= Modifier.size(20.dp))
-
-            }
-        }
+       floatingActionButton= { FloatingActionButton(onClick = { /*TODO*/ }) {
+       }
+       },
+        floatingActionButtonPosition = FabPosition.Center,
+        bottomBar = { BottomNav()}
     )
-    {
+    {}
 
-    }
 }
 
+@Composable
+fun BottomNav()
+{
+    val chosenIndex by remember {mutableStateOf(0)}
+
+    BottomNavigation(elevation = 20.dp,
+            backgroundColor = Color.White)
+    {
+        BottomNavigationItem(icon = {Image(painterResource(id = R.drawable.smile),
+            contentDescription = "smiling emoji" ,
+            modifier= Modifier.size(24.dp),
+            colorFilter = ColorFilter.tint(Color.Black))},
+        label= {Text( "smile")},
+        selected = (chosenIndex.absoluteValue==0),
+            onClick = {(chosenIndex.absoluteValue==0)},
+        )
+
+        BottomNavigationItem(icon={
+            Image(painterResource(id = R.drawable.user),
+                contentDescription = "user" ,
+
+                modifier= Modifier.size(20.dp))
+        },
+            label = {Text(text="profile")},
+            selected = (chosenIndex.absoluteValue==1),
+            onClick = {(chosenIndex.absoluteValue == 1) }
+        )
+
+        }
+}
